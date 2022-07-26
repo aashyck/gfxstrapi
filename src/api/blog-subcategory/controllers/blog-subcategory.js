14260c3a0bb098497c4babe9f799c590a1bef345
Ashiquee <aashyck@gmail.comm>
2022-07-27 01:07:06 +0600

    subcategory

---
+++ src/api/blog-subcategory/controllers/blog-subcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  blog-subcategory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog-subcategory.blog-subcategory');
---
+++ src/api/blog-subcategory/routes/blog-subcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-subcategory router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog-subcategory.blog-subcategory');
